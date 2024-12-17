97c98c382dfbc9e73fcfec9ad73a073db8c4aec9
박종운 <nortiz1349@gmail.com>
2024-12-17 15:09:36 +0900

    macOS v14->13

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MQTTClient",
     platforms: [
         .iOS(.v15),
-        .macOS(.v14)
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
